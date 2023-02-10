e72eb940862c01fb3c194d1e95916cc3169404d2
aleksanchez <sfloriana8@gmail.com>
2023-02-10 11:14:35 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
+//adding event listener & creating element
+addGuestButton.addEventListener("click", function(){
+    let guest = guestInput.value;
+    if (guest !== ""){
+        clearInput();
+        addToList(guest);
+        updateGuestCount();
+    }
+});
+
+//clearing input box
+const clearInput = function(){
+    
+    guestInput.value= "";
+    
+}
+
+//refactoring code
+const addToList = function(guest) {
+    const listItem = document.createElement("li");
+        listItem.innerText = guest;
+        guestList.append(listItem);
+}
+
+//limiting the guest list 
+
+const updateGuestCount = function(){
+    let guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText= guests.length;
+    if (guests.length === 8){
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+}
